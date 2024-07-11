85a6033437ee745e43dabbb35da49de4f3fda575
Mark Simmons <mjsimmons@ucsd.edu>
2024-07-11 12:44:18 -0700

    compare predictions with label

--- scripts/sli.py
+++ scripts/sli.py
@@ -1,8 +1,7 @@
 from transformers import pipeline
 from datasets import load_from_disk, Audio
-from typing import Optional, Sequence
+from typing import Optional, Sequence, Dict, Any, Union, List
 from argparse import ArgumentParser
-import json
 import torch
 import os
 
@@ -32,10 +31,43 @@ def init_argparser() -> ArgumentParser:
     )
     return parser
 
+def compare_predictions(row: Dict[str, Any]):
+    label_col = 'lang'
+    pred_col = 'output'
+
+    label = row[label_col].lower()
+    # TODO: allow using decision threshold for P(ENG)
+    pred = row[pred_col]
+    pred_max = get_max_score(pred)
+    pred_label = pred_max['label']
+
+    # correct if model predicts same label
+    # in practice this will be when model predicts 'eng' for English
+    if label==pred_label:
+        score=1
+    # correct if model predicts non-English for non-English
+    # TODO: allow metalang to be set dynamically
+    elif (label!='eng') and (pred_label!='eng'):
+        score=1
+    # incorrect otherwise
+    else:
+        score=0
+
+    return {
+        "label": label,
+        "pred": pred_label,
+        "score": score,
+    }
+
+def get_max_score(score_list: List[Dict[str, Union[str, float]]]):
+    scores_sorted = sorted(score_list, key=lambda x:x['score'], reverse=True)
+    return scores_sorted[0]
+
 def main(argv: Optional[Sequence[str]]=None) -> int:
     parser = init_argparser()
     args = parser.parse_args(argv)
 
+    # load model and dataset
     pipe = pipeline(
         'audio-classification', 
         args.model,
@@ -43,14 +75,18 @@ def main(argv: Optional[Sequence[str]]=None) -> int:
     )
     split_path = os.path.join(args.dataset, args.split)
     dataset = load_from_disk(split_path)
-
-
     dataset = dataset.cast_column('audio', Audio(sampling_rate=DEFAULT_SR))
 
+    # run inference on dataset using pipeline
     output=pipe(dataset['audio'], batch_size=args.batch_size)
 
-    with open(args.output, 'w') as f:
-        json.dump(output, f)
+    # calculate accuracy on output
+    dataset.add_column("output", output)
+    output_metrics = dataset.map(compare_predictions)
+    output_metrics = output_metrics.to_pandas()
+
+    # save result
+    output_metrics.to_csv(args.output)
 
     return 0
 
