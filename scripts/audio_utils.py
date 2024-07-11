11acab7cbb21e160be1566c8ca315bc7eebd0e7f
Mark Simmons <mjsimmons@ucsd.edu>
2024-07-11 15:33:36 -0700

    initial commit

---
+++ scripts/audio_utils.py
@@ -0,0 +1,8 @@
+from datasets import load_from_disk
+from typing import Optional, Sequence
+
+def main(argv: Optional[Sequence[str]]=None) -> int:
+    return 0
+
+if __name__ == '__main__':
+    main()
