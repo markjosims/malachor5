a470d326397faeca5f92abb9f2e13e2ae2a6382e
Mark Simmons <mjsimmons@ucsd.edu>
2024-07-11 11:22:58 -0700

    allow putting pipeline to device

--- scripts/sli.py
+++ scripts/sli.py
@@ -6,6 +6,7 @@ import json
 
 MMS_LID_256 = 'facebook/mms-lid-256'
 DEFAULT_SR = 16_000
+DEVICE = 0 if torch.cuda.is_available() else -1
 
 def init_argparser() -> ArgumentParser:
     parser = ArgumentParser("Script for running SLI experiment")
@@ -16,7 +17,7 @@ def init_argparser() -> ArgumentParser:
         "--dataset", '-d',
     )
     parser.add_argument(
-        "--device", '-D', type=int,
+        "--device", '-D', type=int, default=DEVICE,
     )
     parser.add_argument(
         '--batch_size', '-b', type=int,
@@ -29,7 +30,11 @@ def main(argv: Optional[Sequence[str]]=None) -> int:
     parser = init_argparser()
     args = parser.parse_args(argv)
 
-    pipe = pipeline('audio-classification', args.model)
+    pipe = pipeline(
+        'audio-classification', 
+        args.model,
+        device=args.device,
+    )
     dataset = load_from_disk(args.dataset)
 
     dataset = dataset.cast_column('audio', Audio(sampling_rate=DEFAULT_SR))
