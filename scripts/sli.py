0667e49a90f6f42340de06cad47d1fad1879afb6
Mark Simmons <marks21@utexas.edu>
2024-07-16 16:56:40 -0700

    detach output

--- scripts/sli.py
+++ scripts/sli.py
@@ -220,9 +220,8 @@ def hf_embeddings(args, dataset) -> torch.Tensor:
         processed_batch = processed_batch.to(torch.device(args.device))
         with torch.no_grad():
             output = model(**processed_batch, output_hidden_states=True)
-        output = output.cpu()
-        output_hs = output['hidden_states']
-        output_lgts = output['logits']
+        output_hs = [hs.cpu() for hs in output['hidden_states']]
+        output_lgts = output['logits'].cpu()
 
         # logits are simple, just append
         logits.append(output_lgts)
