67bf7bacbba4867677084efc3ee42d959c949ca1
Mark Simmons <marks21@utexas.edu>
2024-07-24 16:21:16 -0700

    add arrow to wav conversion as a command argument

--- scripts/audio_utils.py
+++ scripts/audio_utils.py
@@ -1,8 +1,15 @@
 from datasets import load_from_disk
 from typing import Optional, Sequence
+from argparse import ArgumentParser
+
+def init_argparser() -> ArgumentParser:
+    parser = ArgumentParser()
+    parser.add_argument('COMMAND', choices=['ARROW_TO_WAV'])
 
 def main(argv: Optional[Sequence[str]]=None) -> int:
     return 0
+
+
 
 if __name__ == '__main__':
     main()
