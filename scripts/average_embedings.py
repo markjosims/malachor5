3a996ab4f19dc8805dcd01ad59a33fc0740835d4
Mark Simmons <marks21@utexas.edu>
2024-08-27 13:46:46 -0700

    initial commit

---
+++ scripts/average_embedings.py
@@ -0,0 +1,23 @@
+from glob import glob
+import torch
+from argparse import ArgumentParser
+from typing import Optional, Sequence
+
+def init_parser() -> ArgumentParser:
+    parser = ArgumentParser()
+    parser.add_arg('--input', '-i', help='globstr for matching pytorch files.')
+    parser.add_arg('--output', '-o')
+    return parser
+
+def main(argv: Optional[Sequence[str]]) -> int:
+    parser = init_parser()
+    args = parser.parse_args(argv)
+    embeds_dict = {}
+    for pt_file in glob(args.input):
+        embed_matrix = torch.load(pt_file)
+        embed_avg = torch.mean(embed_matrix, dim=0)
+        embeds_dict[pt_file]=embed_avg
+    torch.save(embed_matrix, args.output)
+
+if __name__ == '__main__':
+    main()
