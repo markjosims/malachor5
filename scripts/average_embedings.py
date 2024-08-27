855c47546f79c67cff47498da848363e570d0438
Mark Simmons <marks21@utexas.edu>
2024-08-27 13:47:47 -0700

    add tqdm progress bar

--- scripts/average_embedings.py
+++ scripts/average_embedings.py
@@ -2,6 +2,7 @@ from glob import glob
 import torch
 from argparse import ArgumentParser
 from typing import Optional, Sequence
+from tqdm import tqdm
 
 def init_parser() -> ArgumentParser:
     parser = ArgumentParser()
@@ -13,7 +14,7 @@ def main(argv: Optional[Sequence[str]]) -> int:
     parser = init_parser()
     args = parser.parse_args(argv)
     embeds_dict = {}
-    for pt_file in glob(args.input):
+    for pt_file in tqdm(glob(args.input)):
         embed_matrix = torch.load(pt_file)
         embed_avg = torch.mean(embed_matrix, dim=0)
         embeds_dict[pt_file]=embed_avg
