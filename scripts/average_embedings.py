de2fa0a5d8af029ba669e55be7a78b8de7a8f587
Mark Simmons <marks21@utexas.edu>
2024-08-27 13:58:54 -0700

    delete embed matrix after calculating average

--- scripts/average_embedings.py
+++ scripts/average_embedings.py
@@ -18,6 +18,7 @@ def main(argv: Optional[Sequence[str]]=None) -> int:
     for pt_file in tqdm(glob(args.input)):
         embed_matrix = torch.load(pt_file, map_location=torch.device(args.device))
         embed_avg = torch.mean(embed_matrix, dim=0).to('cpu')
+        del embed_matrix
         embeds_dict[pt_file]=embed_avg
     torch.save(embeds_dict, args.output)
 
