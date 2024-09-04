bab2d1ab885d1c7efb0b843271da7b5689e50bd9
Mark Simmons <marks21@utexas.edu>
2024-09-04 09:23:23 -0700

    remove unused import

--- scripts/asr_dataset.py
+++ scripts/asr_dataset.py
@@ -15,9 +15,8 @@ import torch
 import torchaudio
 from allosaurus.app import read_recognizer
 from tempfile import TemporaryDirectory
-from clap.encoders import SpeechEncoder, PhoneEncoder
+from clap.encoders import SpeechEncoder, PhoneEncoder # type: ignore
 from scipy.io.wavfile import read, write
-from scipy.interpolate import interp1d
 
 GDRIVE_DIR = '/Users/markjos/Library/CloudStorage/GoogleDrive-mjsimmons@ucsd.edu/Shared drives/Tira/Recordings'
 DEVICE = 0 if torch.cuda.is_available() else 'cpu'
