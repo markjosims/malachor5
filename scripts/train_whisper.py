95d0573fd85b4a04d9ccd6e9aeb47e99f5a5fe99
Mark Simmons <mjsimmons@ucsd.edu>
2024-09-17 11:37:19 -0700

    initial commit

---
+++ scripts/train_whisper.py
@@ -0,0 +1,12 @@
+from argparse import ArgumentParser
+from typing import Sequence, Optional
+
+def init_parser() -> ArgumentParser:
+    parser = ArgumentParser()
+    return parser
+
+def main(argv: Sequence[Optional[str]]=None) -> int:
+    return 0
+
+if __name__ == '__main__':
+    main()
