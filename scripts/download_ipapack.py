103eb8988723a825c44368301e3534436883fde5
Mark Simmons <marks21@utexas.edu>
2024-09-25 09:29:24 -0700

    script to download IPAPACK

---
+++ scripts/download_ipapack.py
@@ -0,0 +1,25 @@
+from huggingface_hub import snapshot_download
+import os
+
+DATASET_DIR = os.environ.get('DATASET_DIR', '/usr/markjos/datasets')
+
+def main():
+    datasets = [
+        "fleurs_ipa",
+        "mswc_ipa",
+        "doreco_ipa",
+    ]
+    user="anyspeech/"
+
+    for dataset in datasets:
+        snapshot_download(
+            repo_id=user+dataset,
+            repo_type="dataset",
+            local_dir=os.path.join(DATASET_DIR, dataset),
+            local_dir_use_symlinks=False,
+            resume_download=False,
+            max_workers=4
+        )
+
+if __name__ == '__main__':
+    main()
