f6e5cb2a9589212c65238f6cb8f6f3daeb3a7a38
Mark Simmons <marks21@utexas.edu>
2024-10-03 10:26:18 -0700

    add peft arg

--- scripts/asr_dataset.py
+++ scripts/asr_dataset.py
@@ -88,6 +88,7 @@ def init_parser() -> ArgumentParser:
     infer_vad_parser = commands.add_parser('infer_vad', help=infer_vad.__doc__)
     infer_vad_parser.add_argument('--model', '-m', default='pyannote/voice-activity-detection')
     infer_vad_parser.add_argument('--device', '-D', type=device_type, default=DEVICE,)
+    infer_vad_parser.add_argument('--peft', action='store_true')
     infer_vad_parser.set_defaults(func=infer_vad)
 
     infer_allosaurus_parser = commands.add_parser('infer_allosaurus', help=infer_allosaurus.__doc__)
