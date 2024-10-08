f0f7ee3cc3c7f25f61cbb034cb41bfe8b08d7292
Mark Simmons <marks21@utexas.edu>
2024-10-08 00:45:10 -0700

    fix default ['train'] to str 'train'

--- scripts/train_whisper.py
+++ scripts/train_whisper.py
@@ -60,7 +60,7 @@ def init_parser() -> ArgumentParser:
     parser.add_argument('--load_ds_cache', '-c', action='store_true')
     parser.add_argument('--resume_from_checkpoint' ,action='store_true')
     parser.add_argument('--checkpoint')
-    parser.add_argument('--action', choices=['train', 'evaluate', 'test'], default=['train'])
+    parser.add_argument('--action', choices=['train', 'evaluate', 'test'], default='train')
     parser.add_argument('--eval_output')
     parser.add_argument('--char_vocab')
     parser.add_argument('--condense_tones', action='store_true')
