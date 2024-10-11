6360e32103bc306fcad39482227899f24ae9e856
Mark Simmons <marks21@utexas.edu>
2024-10-11 16:14:51 -0700

    initial commit

---
+++ scripts/add_elan_annotations.py
@@ -0,0 +1,6 @@
+"""
+For all .eaf files in `args.output`, find an eaf file in `args.input` with the same basename
+and add all tiers from the input .eaf file.
+If `args.media` is specified, add a linked file pointing to the respective wav in the media dir.
+"""
+
