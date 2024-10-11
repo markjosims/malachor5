d5b23a28ba95a2ed29d5df741ce7393b98636b2e
Mark Simmons <marks21@utexas.edu>
2024-10-11 16:17:09 -0700

    boilerplate

--- scripts/add_elan_annotations.py
+++ scripts/add_elan_annotations.py
@@ -2,6 +2,7 @@ from argparse import ArgumentParser
 from pympi.Elan import Eaf
 from glob import glob
 import os
+from typing import Optional, Sequence
 
 """
 For all .eaf files in `args.output`, find an eaf file in `args.input` with the same basename
@@ -9,3 +10,19 @@ and add all tiers from the input .eaf file.
 If `args.media` is specified, add a linked file pointing to the respective wav in the media dir.
 """
 
+# ---- #
+# main #
+# ---- #
+
+def init_parser() -> ArgumentParser:
+    parser = ArgumentParser()
+
+
+def main(argv:Optional[Sequence[str]]=None) -> int:
+    parser = init_parser()
+    args=parser.parse_args(argv)
+
+    return 0
+
+if __name__ == '__main__':
+    main()
