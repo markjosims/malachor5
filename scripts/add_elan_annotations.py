4dc84055d2fea32044cefc7681cb24c0fdc1fa51
Mark Simmons <marks21@utexas.edu>
2024-10-11 16:27:18 -0700

    map eafs and wavs to stem

--- scripts/add_elan_annotations.py
+++ scripts/add_elan_annotations.py
@@ -1,8 +1,9 @@
 from argparse import ArgumentParser
 from pympi.Elan import Eaf
 from glob import glob
-import os
+from pathlib import Path
 from typing import Optional, Sequence
+from collections import defaultdict
 
 """
 For all .eaf files in `args.output`, find an eaf file in `args.input` with the same basename
@@ -25,12 +26,28 @@ def init_parser() -> ArgumentParser:
 def main(argv:Optional[Sequence[str]]=None) -> int:
     parser = init_parser()
     args=parser.parse_args(argv)
+    media=[]
     if args.recursive:
-        in_eafs = glob(os.path.join(args.input, '**', '*.wav'), recursive=True)
-        out_eafs = glob(os.path.join(args.output, '**', '*.wav'), recursive=True)
+        in_eafs = glob(Path(args.input)/'**/*.eaf', recursive=True)
+        out_eafs = glob(Path(args.output)/'**/*.eaf', recursive=True)
+        if args.media:
+            media = glob(Path(args.media)/'**/*.wav', recursive=True)
     else:
-        in_eafs = glob(os.path.join(args.input, '*.wav'))
-        out_eafs = glob(os.path.join(args.output, '*.wav'))
+        in_eafs = glob(Path(args.input)/'*.eaf')
+        out_eafs = glob(Path(args.output)/'*.eaf')
+        if args.media:
+            media = glob(Path(args.media)/'**/*.wav', recursive=True)
+    eaf_map=defaultdict(dict)
+    for eaf_fp in in_eafs:
+        stem=Path(eaf_fp).stem
+        eaf_map[stem]['in_eaf']=eaf_fp
+    for eaf_fp in out_eafs:
+        stem=Path(eaf_fp).stem
+        eaf_map[stem]['out_eaf']=eaf_fp
+    for wav_fp in media:
+        stem=Path(wav_fp).stem
+        eaf_map[stem]['media']=wav_fp
+
 
     return 0
 
