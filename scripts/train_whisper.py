9703d8df215a7aafea058f57ba141d13c1a7abdc
Mark Simmons <mjsimmons@ucsd.edu>
2024-10-14 14:41:28 -0700

    make dir at eval_output_stem

--- scripts/train_whisper.py
+++ scripts/train_whisper.py
@@ -348,6 +348,7 @@ def evaluate_all_checkpoints(args, ds, processor, training_args, compute_metrics
             if int(chkpnt.removesuffix('/').split(sep='-')[-1]) in args.chkpnts
         ]
     eval_output_stem=args.eval_output or args.output
+    os.makedirs(eval_output_stem, exist_ok=True)
     metrics=[]
     for chkpnt in tqdm(chkpnts, desc='Evaluating checkpoints'):
         chkpnt=chkpnt.removesuffix('/')
