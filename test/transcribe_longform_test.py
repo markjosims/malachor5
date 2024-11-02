5e4353587ea9d4856d49b28d32f7062ca0b0edbb
Mark Simmons <marks21@utexas.edu>
2024-11-01 20:52:39 -0700

    implement `test_asr`

--- test/transcribe_longform_test.py
+++ test/transcribe_longform_test.py
@@ -4,7 +4,7 @@ from test_utils import assert_chunk_dict_shape
 
 import sys
 sys.path.append('scripts')
-from transcribe_longform import perform_vad, load_and_resample
+from transcribe_longform import perform_vad, perform_asr, load_and_resample
 SAMPLE_WAVPATH = 'test/data/sample_biling.wav'
 
 def test_load_and_resample():
@@ -17,9 +17,21 @@ def test_load_and_resample():
 
 def test_vad():
     """
-    `perform_vad` should return a dict with key 'chunks' which maps to
+    `perform_vad` should return a dict with key 'vad_chunks' which maps to
     a list of dicts each with a `timestamps` key mapping to a 2-tuple of floats
     """
     wav = load_and_resample(SAMPLE_WAVPATH)
     vad_out = perform_vad(wav)
     assert_chunk_dict_shape(vad_out, 'vad_chunks')
+
+def test_asr():
+    """
+    `perform_asr` should return a dict with key `text` mapping to a str
+    and key 'chunks' which mapping to a list of dicts each with `timestamps`
+    and `text` keys
+    """
+    wav = load_and_resample(SAMPLE_WAVPATH)
+    asr_out = perform_asr(wav, model_path='openai/whisper-tiny')
+    assert_chunk_dict_shape(asr_out)
+    assert 'text' in asr_out
+    assert type(asr_out['text']) is str
