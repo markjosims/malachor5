77f2e6c2e7a0d1bf535cb2ca4316761c2da19a6a
Mark Simmons <marks21@utexas.edu>
2024-11-04 10:32:58 -0600

    implement `test_dataset_language`

---
+++ test/dataset_utils_test.py
@@ -0,0 +1,25 @@
+from argparse import Namespace
+from test_utils import assert_tokens_in_row
+
+import sys
+sys.path.append('scripts')
+from dataset_utils import load_and_prepare_dataset
+
+TIRA_ASR_DS = 'data/pyarrow-datasets/tira-asr-hf'
+SPECIAL_TOKENS = {
+    'en':           {'token': '<|en|>',                 'id': 50259},
+    'sw':           {'token': '<|sw|>',                 'id': 50318},
+    'bos':          {'token': '<|startoftranscript|>',  'id': 50258},
+    'eos':          {'token': '<|endoftext|>',          'id': 50257},
+    'notimestamps': {'token': '<|notimestamps|>',       'id': 50363},
+    'transcribe':   {'token': '<|transcribe|>',         'id': 50359},
+}
+
+def test_dataset_language():
+    args = Namespace(
+        dataset=TIRA_ASR_DS,
+        language='en',
+    )
+    ds = load_and_prepare_dataset(args)
+    ds.map(lambda row: assert_tokens_in_row(row, languages=['en']))
+    
