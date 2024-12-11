49a5c1a0b2edea53108182a89a71a27d3d12ed1f
Mark Simmons <marks21@utexas.edu>
2024-12-11 13:24:55 -0800

    add empty method `test_train_logreg`

--- test/sli_test.py
+++ test/sli_test.py
@@ -1,8 +1,12 @@
 import sys
 sys.path.append('scripts')
-from sli import load_lr, infer_lr
+from sli import train_logreg, infer_lr
+from model_utils import load_lr
 
 def test_load_lr():
+    ...
+
+def test_train_logreg():
     ...
 
 def test_infer_lr():
