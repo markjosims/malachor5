4f6091ad4441f5d26b5c76029a893237fb015c6c
Mark Simmons <marks21@utexas.edu>
2024-12-18 10:50:00 -0600

    add docstr

--- scripts/drz_eval.py
+++ scripts/drz_eval.py
@@ -3,4 +3,13 @@ from pyannote.core import Annotation, Segment, Timeline
 from typing import Union, Dict
 
 def elan_to_pyannote(eaf: Union[str, Elan.Eaf]) -> Dict[str, Annotation]:
+    """
+    Returns a dict of pyannote `Annotation` objects from an Elan file.
+    The label for each annotation segment is the language being spoken for that interval.
+    Creates one annotation for each speaker tier (where speaker tiers are
+    are assumed to be those whose names are three capital letters, e.g. HIM or MAR),
+    as well as 'combined' and 'verbose' `Annotation` objects, both of which
+    pool annotations from all speakers into the same timeline, but where 'combined'
+    only includes the language label and `verbose` includes both language and speaker label.
+    """
     ...
