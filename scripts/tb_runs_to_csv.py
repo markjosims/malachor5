2881af795d3e05c72a720fe410d6ccb0f9e4462c
Mark Simmons <marks21@utexas.edu>
2025-02-11 17:33:08 -0800

    skip empty dfs

--- scripts/tb_runs_to_csv.py
+++ scripts/tb_runs_to_csv.py
@@ -85,7 +85,7 @@ def latest_run_per_event(df: pd.DataFrame):
         for step in df['step'].unique():
             step_mask = df['step']==step
             step_tag_df = df[step_mask&tag_mask]
-            if len(step_tag_df)==1:
+            if len(step_tag_df)==0:
                 continue
             step_tag_df = step_tag_df.sort_values('date', ascending=False)
             latest_date = step_tag_df.iloc[0].index
