e8a09a3659ad50ef418b0b0617d1dececd9a7e88
markjosims <mjsimmons@ucsd.edu>
2025-04-15 08:25:03 -0700

    unpack encoder input

--- scripts/kws.py
+++ scripts/kws.py
@@ -2,7 +2,9 @@ from typing import Union, List
 from model_utils import DEVICE
 from clap.encoders import SpeechEncoder, PhoneEncoder
 from transformers import DebertaV2Tokenizer, AutoProcessor
+from longform import load_and_resample
 import torch
+import numpy as np
 
 def embed_speech(audio: Union[str, List[str], torch.Tensor]) -> torch.Tensor:
     speech_encoder = SpeechEncoder.from_pretrained('anyspeech/clap-ipa-tiny-speech')
@@ -10,6 +12,9 @@ def embed_speech(audio: Union[str, List[str], torch.Tensor]) -> torch.Tensor:
 
     processor = AutoProcessor.from_pretrained('openai/whisper-tiny')
 
+    if type(audio) is str:
+        audio = load_and_resample(audio)
+    audio = audio.squeeze()
     audio_input = processor(
         audio,
         sampling_rate=16_000, # these kwargs avoid bugs
@@ -19,7 +24,7 @@ def embed_speech(audio: Union[str, List[str], torch.Tensor]) -> torch.Tensor:
     audio_input=audio_input.to(DEVICE)
 
     with torch.no_grad():
-        speech_embed = speech_encoder(audio_input)['pooler_output']
+        speech_embed = speech_encoder(**audio_input)['pooler_output']
     return speech_embed
 
 def embed_text(text: Union[str, List[str]]) -> torch.Tensor:
@@ -37,5 +42,5 @@ def embed_text(text: Union[str, List[str]]) -> torch.Tensor:
     ipa_input=ipa_input.to(DEVICE)
 
     with torch.no_grad():
-        phone_embed = phone_encoder(ipa_input)['pooler_output']
+        phone_embed = phone_encoder(**ipa_input)['pooler_output']
     return phone_embed
