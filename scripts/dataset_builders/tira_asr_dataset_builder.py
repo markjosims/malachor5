2ebf4d7c14480d2f87f25045fa5d6e1f677ab2d5
Mark Simmons <mjsimmons@ucsd.edu>
2025-04-28 10:41:47 -0700

    use `has_diac` function to detect toneless words

--- scripts/dataset_builders/tira_asr_dataset_builder.py
+++ scripts/dataset_builders/tira_asr_dataset_builder.py
@@ -1,13 +1,13 @@
-from argparse import ArgumentParser
 from typing import Sequence, Optional
 from tira_elan_scraper import LIST_PATH
 import os
 import pandas as pd
 from string import Template
+from dataset_builder_utils import get_readable_duration
 
 import sys
 sys.path.append('scripts')
-from string_norm import unicode_normalize, strip_diacs, remove_punct
+from string_norm import unicode_normalize, has_diac, remove_punct
 
 AUDIO_DIR = os.environ.get("TIRA_ELICITATION_WAVS")
 TIRA_ASR_CLIPS_DIR = os.environ.get("TIRA_ASR_CLIPS")
@@ -40,7 +40,8 @@ def main(argv: Optional[Sequence[str]]=None) -> int:
 
     # drop na rows
     df=df.dropna()
-    nan_str = f"- {len(df)} non-NaN transcriptions in dataset"
+    get_df_duration = lambda: get_readable_duration(df['duration'].sum())
+    nan_str = f"- {len(df)} non-NaN transcriptions in dataset, {get_df_duration()}"
     print(nan_str)
     PREPROCESSING_STEPS.append(nan_str)
 
@@ -52,26 +53,14 @@ def main(argv: Optional[Sequence[str]]=None) -> int:
 
     # skip all toneless entries
     print("Dropping rows with no tone diacritics")
-    no_diac_mask = df["text"].apply(strip_diacs) == df["text"]
+    has_tone_mask = df["text"].apply(lambda s: has_diac(s, tone_only=True))
     prev_len = len(df)
-    df = df[~no_diac_mask]
+    df = df[has_tone_mask]
     toneless_rows = prev_len-len(df)
-    toneless_str = f"- removed {toneless_rows} rows with no tone marked, {len(df)} rows remaining"
+    toneless_str = f"- removed {toneless_rows} rows with no tone marked, {len(df)} rows remaining, {get_df_duration()}"
     print(toneless_str)
     PREPROCESSING_STEPS.append(toneless_str)
 
-    tone_markers = {
-        'grave': "\u0300",
-        'macrn': "\u0304",
-        'acute': "\u0301",
-        'circm': "\u0302",
-        'caron': "\u030C",
-    }
-    has_tone = lambda s: any(diac in s for diac in tone_markers.values())
-
-    df_tone=df.copy()[df['text'].apply(has_tone)]
-    print(len(df_tone), len(df), len(df)-len(df_tone))
-    # for some reason the first step is missing 72 toneless rows!
 
 if __name__ == '__main__':
     main()
