19b2e3facc539b6cdf9f0332b6523c2825a95461
Mark Simmons <mjsimmons@ucsd.edu>
2025-04-28 11:22:00 -0700

    `is_en_word` allows for setting frequency threshold and rejecting non-ascii words

--- scripts/lid_utils.py
+++ scripts/lid_utils.py
@@ -23,8 +23,18 @@ with open(zulu_words_path, encoding='utf8') as f:
 # ---------------- #
 
 @strip_punct
-def is_en_word(w: str) -> bool:
-    return wordfreq.word_frequency(w, 'en')>0
+def is_en_word(w: str, expect_ascii: bool = True, threshold=1e-9) -> bool:
+    """
+    Returns True if a word is detected as English, False otherwise.
+    Default behavior is to return True if word is recognized by `wordfreq`.
+    Frequency must be greater than `threshold` value to prevent ultra
+    low-frequency words from slipping through. If `expect_ascii=True`,
+    return False if non-ascii unicode characters are detected, regardless
+    of `wordfreq` search.
+    """
+    if expect_ascii and has_unicode(w):
+        return True
+    return wordfreq.word_frequency(w, 'en')>threshold
 
 @strip_punct
 def has_unicode(s):
