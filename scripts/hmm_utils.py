e8e97eaa2f68bd4e592271ce10c536559781a165
Mark Simmons <mjsimmons@ucsd.edu>
2025-04-29 14:23:44 -0700

    implement `calculate_transition_probs`

--- scripts/hmm_utils.py
+++ scripts/hmm_utils.py
@@ -2,6 +2,10 @@ from pomegranate.distributions._distribution import Distribution
 from pomegranate._utils import _cast_as_tensor
 import torch
 from typing import *
+from nltk import ngrams, FreqDist
+
+BOS = '<s>'
+EOS = '</s>'
 
 class EmbeddingSimilarity(Distribution):
     """
@@ -50,3 +54,130 @@ class KeySimilarityMatrix(Distribution):
     
     def _reset_cache(self):
         return
+    
+def tag_sentence(sentence: str) -> str:
+    """
+    Given a string of a sentence, tag the first word with '_S' for 'start',
+    the last word with '_F' for 'final', and all non-terminal words with '_1'
+    unless the word occurs after itself, in which case tag the first instance with '_1',
+    the second with '_2' and so on.
+    If a sentence only has one word, tag word with '_O' for 'only'.
+    """
+    words = sentence.split()
+    if len(words)==1:
+        return words[0]+'_O'
+    words[0]+='_S'
+    words[-1]+='_F'
+    for i in range(1, len(words)-1):
+        word = words[i]
+        prev_word, prev_tag = words[i-1].split('_')
+        if (word == prev_word) and (prev_tag != 'S'):
+            prev_tag_val = int(prev_tag)
+            words[i] = f"{word}_{prev_tag_val+1}"
+        else:
+            words[i] = word+'_1'
+    return ' '.join(words)
+    
+def is_non_terminal(word: str) -> bool:
+    if word in [BOS, EOS]:
+        return False
+    if word.split('_')[-1] in ['S', 'F']:
+        return False
+    return True
+
+def calculate_transition_probs(
+        keyphrase_list: List[str],
+        enter_prob: float = 0.1,
+        self_trans_prob: float = 0.5,
+        early_exit_prob: float = 0.01,
+        late_enter_prob: float = 0.01,
+        non_keyword_states: List[str] = ['SIL', 'SPCH']
+    ) -> List[Tuple[str, str, float]]:
+    """
+    Calculates transition probabilities between words given a list of keyphrases.
+    Returns a list of tuples of shape ('$start_word', '$end_word', $weight).
+    `non_keyword_states` indicates the names of all non-keyword states, by default
+    'SIL' for silence/non-speech and 'SPCH' for (non-keyword) speech.
+    """
+    transitions = []
+    continue_prob = 1-(self_trans_prob+early_exit_prob)
+
+    keyphrases_tagged = [tag_sentence(keyphrase) for keyphrase in keyphrase_list]
+    bigrams = []
+    unigrams = []
+    for keyphrase in keyphrases_tagged:
+        bigrams.extend(
+            ngrams(
+                keyphrase.split(),
+                2,
+                pad_left=True,
+                pad_right=True,
+                left_pad_symbol=BOS,
+                right_pad_symbol=EOS
+            )
+        )
+        unigrams.append(BOS)
+        unigrams.extend(keyphrase.split())
+        unigrams.append(EOS)
+    bigram_cts = FreqDist(bigrams)
+    unigram_cts = FreqDist(unigrams)
+    # add bigram transitions
+    for bigram, bigram_ct in bigram_cts.items():
+        start_word, end_word = bigram
+        start_word_ct = unigram_cts[start_word]
+        trans_prob = bigram_ct/start_word_ct # Prob(end_word|start_word)
+
+        if start_word == BOS:
+            # transition into keyphrase
+            transition_weight = trans_prob*enter_prob
+            for non_keyword_state in non_keyword_states:
+                transitions.append((non_keyword_state, end_word, transition_weight))
+        elif end_word == EOS:
+            # transition out of keyphrase
+            # ignore `trans_prob`, instead scale by number of non-keyword states
+            transition_weight = continue_prob*(1/len(non_keyword_states))
+            for non_keyword_state in non_keyword_states:
+                transitions.append((start_word, non_keyword_state, transition_weight))
+        else:
+            # transition within keyphrase
+            transition_weight = trans_prob*continue_prob
+            transitions.append((start_word, end_word, transition_weight))
+    
+    # add self-transition, late entry and early exit probs
+    non_initial_words = [word for word in unigrams if is_non_terminal(word) or word.endswith('_F')]
+    non_initial_ct = len(non_initial_words)
+    for unigram, unigram_ct in unigram_cts.items():
+        # Prob(unigram|unigram)
+        # `self_trans_prob` only applies to words
+        # non-keyword states transition to self or each other with equal probability
+        if unigram == BOS:
+            non_enter_prob = 1-(enter_prob+late_enter_prob)
+            self_trans_weight = non_enter_prob/len(non_keyword_states)
+            for start_state in non_keyword_states:
+                for end_state in non_keyword_states:
+                    transitions.append((start_state, end_state, self_trans_weight))
+        elif unigram == EOS:
+            # only do self-transitions of BOS to avoid duplicate transitions
+            # since non-keyword states are same before and after sentence
+            pass
+        else:
+            transitions.append((unigram, unigram, self_trans_prob))
+
+        # late entry applies to non-initial words
+        if is_non_terminal(unigram) or unigram.endswith('_F'):
+            # Prob(unigram|non_keyword_state)
+            late_enter_weight = late_enter_prob * unigram_ct/non_initial_ct
+            for non_keyword_state in non_keyword_states:
+                transitions.append((non_keyword_state, unigram, late_enter_weight))
+        # early exit only apply to non-final words
+        if is_non_terminal(unigram) or unigram.endswith('_S'):
+            # Prob(non_keyword_state|unigram)
+            early_exit_weight = early_exit_prob / len(non_keyword_states)
+            for non_keyword_state in non_keyword_states:
+                transitions.append((unigram, non_keyword_state, early_exit_weight))
+        
+
+
+    return transitions
+    
+    
--- test/hmm_test.py
+++ test/hmm_test.py
@@ -1,6 +1,6 @@
 import sys
 sys.path.append('scripts')
-from hmm_utils import EmbeddingSimilarity, KeySimilarityMatrix
+from hmm_utils import EmbeddingSimilarity, KeySimilarityMatrix, calculate_transition_probs, tag_sentence
 from kws import get_similarity_matrix
 import torch
 
@@ -45,3 +45,105 @@ def test_key_similarity_distribution():
     assert keysim2_probs[0]>keysim2_probs[2]
     assert keysim2_probs.argmax(0)==1
     assert keysim2_probs.argmin(0)==2
+
+def test_tag_tokens():
+    keyphrase_list = [
+        "foo foo foo foo foo",
+        "foo bar bar baz",
+        "bar baz baz bar"
+    ]
+    expected_tagged_keyphrases = [
+        "foo_S foo_1 foo_2 foo_3 foo_F",
+        "foo_S bar_1 bar_2 baz_F",
+        "bar_S baz_1 baz_2 bar_F",
+    ]
+    tagged_keyphrases = [tag_sentence(sentence) for sentence in keyphrase_list]
+    assert expected_tagged_keyphrases == tagged_keyphrases
+
+def test_calculate_transition_probs():
+    keyphrase_list = [
+        "foo foo foo",
+        "foo bar",
+        "bar baz"
+    ]
+    entr_prob = 0.1
+    skip_prob = 0.01
+    exit_prob = 0.01
+    self_prob = 0.5
+    cont_prob = 1-(exit_prob+self_prob)
+    transition_probs = calculate_transition_probs(
+        keyphrase_list,
+        enter_prob=entr_prob,
+        self_trans_prob=self_prob,
+        early_exit_prob=exit_prob,
+        late_enter_prob=skip_prob,
+    )
+    expected_transitions = [
+        ("SIL",   "SIL",   (1/2)*(1-(entr_prob+skip_prob))),
+        ("SIL",   "SPCH",  (1/2)*(1-(entr_prob+skip_prob))),
+        ("SPCH",  "SPCH",  (1/2)*(1-(entr_prob+skip_prob))),
+        ("SPCH",  "SIL",   (1/2)*(1-(entr_prob+skip_prob))),
+
+        ("SIL",   "foo_S", (2/3)*entr_prob),
+        ("SPCH",  "foo_S", (2/3)*entr_prob),
+        ("foo_S", "foo_S", self_prob),
+        ("foo_S", "foo_1", (1/2)*cont_prob),
+        ("foo_S", "bar_F", (1/2)*cont_prob),
+        ("foo_S", "SIL",   (1/2)*exit_prob),
+        ("foo_S", "SPCH",  (1/2)*exit_prob),
+
+        ("SIL",   "foo_1", (1/4)*skip_prob),
+        ("SPCH",  "foo_1", (1/4)*skip_prob),
+        ("foo_1", "foo_1", self_prob),
+        ("foo_1", "foo_F", (1/1)*cont_prob),
+        ("foo_1", "SIL",   (1/2)*exit_prob),
+        ("foo_1", "SPCH",  (1/2)*exit_prob),
+        
+        ("SIL",   "foo_F", (1/4)*skip_prob),
+        ("SPCH",  "foo_F", (1/4)*skip_prob),
+        ("foo_F", "foo_F", self_prob),
+        ("foo_F", "SIL",   (1/2)*cont_prob),
+        ("foo_F", "SPCH",  (1/2)*cont_prob),
+
+        ("SIL",   "bar_F", (1/4)*skip_prob),
+        ("SPCH",  "bar_F", (1/4)*skip_prob),
+        ("bar_F", "bar_F", self_prob),
+        ("bar_F", "SIL",   (1/2)*cont_prob),
+        ("bar_F", "SPCH",  (1/2)*cont_prob),
+
+        ("SIL",   "bar_S", (1/3)*entr_prob),
+        ("SPCH",  "bar_S", (1/3)*entr_prob),
+        ("bar_S", "bar_S", self_prob),
+        ("bar_S", "baz_F", (1/1)*cont_prob),
+        ("bar_S", "SIL",   (1/2)*exit_prob),
+        ("bar_S", "SPCH",  (1/2)*exit_prob),
+
+        ("SIL",   "baz_F", (1/4)*skip_prob),
+        ("SPCH",  "baz_F", (1/4)*skip_prob),
+        ("baz_F", "baz_F", self_prob),
+        ("baz_F", "SIL",   (1/2)*cont_prob),
+        ("baz_F", "SPCH",  (1/2)*cont_prob),
+    ]
+
+    bigrams = [t[:2] for t in transition_probs]
+    expected_bigrams = [t[:2] for t in expected_transitions]
+    missing_bigrams = [t for t in expected_bigrams if t not in bigrams]
+    extra_bigrams = [t for t in bigrams if t not in expected_bigrams]
+
+    # uncomment for debugging
+    # duplicate_bigrams = [b for b in bigrams if bigrams.count(b)>1]
+    # duplicate_transitions = [t for t in transition_probs if t[:2] in duplicate_bigrams]
+    # extra_transitions = [t for t in transition_probs if t not in expected_transitions]
+
+    assert len(missing_bigrams)==0
+    assert len(extra_bigrams)==0
+
+
+    assert len(transition_probs) == len(expected_transitions)
+    for expected_transition in expected_transitions:
+        bigram = expected_transition[:2]
+        transition = [t for t in transition_probs if t[:2]==bigram]
+        assert len(transition)==1
+        transition_weight = transition[0][-1]
+        expected_weight = expected_transition[-1]
+        assert transition_weight == expected_weight
