6352cef90e29c853025717fd7790445c9cb1d7ce
Mark Simmons <mjsimmons@ucsd.edu>
2025-05-29 10:09:20 -0700

    embed speech and text

--- scripts/dataset_builders/audio_segment_ds_builder.py
+++ scripts/dataset_builders/audio_segment_ds_builder.py
@@ -8,7 +8,15 @@ import pandas as pd
 
 import sys
 sys.path.append('scripts')
-from kws import get_sliding_window, textgrid_to_df
+from kws import (
+    get_sliding_window,
+    textgrid_to_df,
+    dataloader,
+    embed_speech,
+    embed_text,
+    SpeechEncoder,
+    PhoneEncoder,
+)
 
 VERSION = "0.1.0"
 
@@ -75,17 +83,23 @@ def get_agg_tg_df(tg_paths):
 def main(argv: Optional[Sequence[str]]=None) -> int:
     parser = ArgumentParser()
     parser.add_argument("--dataset", "-d")
+    parser.add_argument("--output", '-o')
     parser.add_argument("--textgrid_dir")
+    parser.add_argument("--batch_size", '-b', default=128)
     parser.add_argument("--framelengths", nargs="+", type=float, default=[0.020, 0.500])
     parser.add_argument("--frameshifts", nargs="+", type=float, default=[0.015, 0.400])
     args = parser.parse_args(argv)
     ds = load_from_disk(args.dataset)
     if type(ds) is DatasetDict:
         ds = ds['train']
+
+    # get sliding windows
     window_ds_list = []
     ds.map(
         lambda row: get_windows(row, args.framelengths, args.frameshifts, window_ds_list),
     )
+
+    # get phone/word intervals from textgrids
     tg_paths = glob(os.path.join(args.textgrid_dir, '*.TextGrid'))
     tg_df=get_agg_tg_df(tg_paths)
     ds.map(
@@ -93,7 +107,58 @@ def main(argv: Optional[Sequence[str]]=None) -> int:
     )
 
     window_ds = concatenate_datasets(window_ds_list)
-    breakpoint()
+    outdir = args.output or args.dataset+'_frames'
+
+    # get speech embeddings
+    audio_clips = [row['audio']['array'] for row in window_ds]
+    speech_encoder = SpeechEncoder.from_pretrained('anyspeech/clap-ipa-small-speech')
+    speech_embeds = []
+    for batch in dataloader(audio, args.batch_size):
+        batch_embeds = embed_speech(batch, speech_encoder)
+        speech_embeds.extend(batch_embeds.cpu())
+        del batch_embeds
+    del speech_encoder
+    window_ds = window_ds.add_column('clap_embed_speech', speech_embeds)
+
+    speech_aligner = SpeechEncoder.from_pretrained('anyspeech/ipa-align-small-speech')
+    speech_embeds = []
+    for batch in dataloader(audio, args.batch_size):
+        batch_embeds = embed_speech(batch, speech_aligner)
+        speech_embeds.extend(batch_embeds.cpu())
+        del batch_embeds
+    del speech_aligner
+    window_ds = window_ds.add_column('align_embed_speech', speech_embeds)
+
+    # get text embeddings
+    # since num(unique text strs) << num(rows), create a memo of embeds
+    # for each unique str and map to dataset rows
+    unique_text = set(window_ds['text'])
+    if '' in unique_text:
+        unique_text.remove('')
+    text2embed = {'': None}
+    text_encoder = PhoneEncoder.from_pretrained('anyspeech/clap-ipa-small-phone')
+    for batch in dataloader(unique_text):
+        batch_embeds = embed_text(batch)
+        batch_embeds = batch_embeds.cpu()
+        text2embed.update(k: batch_embeds[i] for k, i in enumerate(batch))
+    del text_encoder
+
+    text2embed_align = {'': None}
+    text_aligner = PhoneEncoder.from_pretrained('anyspeech/ipa-align-small-phone')
+    for batch in dataloader(unique_text):
+        batch_embeds = embed_text(batch)
+        batch_embeds = batch_embeds.cpu()
+        text2embed_align.update(k: batch_embeds[i] for k, i in enumerate(batch))
+    del text_aligner
+
+    window_ds = window_ds.map(
+        lambda row: {
+            "clap_embed_text": text2embed[row['text']],
+            "align_embed_text": text2embed_align[row['text']],
+        }
+    )
+    window_ds.save_to_disk(outdir)
+
     return 0
 
 if __name__ == '__main__':
